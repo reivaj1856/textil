6a6a72c07b126935dc61f71d6b2a951bcf3c905f
reivaj1856 <javier.cruz.mt.1856@gmail.com>
2025-05-10 10:14:35 -0400

    structur basic

---
+++ src/app/auth/features/auth-login/auth-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-login',
+  imports: [],
+  templateUrl: './auth-login.component.html',
+  styleUrl: './auth-login.component.css'
+})
+export class AuthLoginComponent {
+
+}
---
+++ src/app/auth/features/auth-register/auth-register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-register',
+  imports: [],
+  templateUrl: './auth-register.component.html',
+  styleUrl: './auth-register.component.css'
+})
+export class AuthRegisterComponent {
+
+}
